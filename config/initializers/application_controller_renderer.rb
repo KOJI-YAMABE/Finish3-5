882af78613db803f7a4e8e3b15878829b835b1b6
KOJI-YAMABE <kvbl.ak25@gmail.com>
2020-08-06 16:21:13 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eeabd766cf0dc9b95c58e15a7e88d666925efb470c20059d72a22755d928ff4e7c4167ab42d8fcc8b8a6bd0892cce195ebcab8ae36b95debcd48a153051e3da5'
